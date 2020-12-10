1716abd45c6f62e71ed60d2e08cdcdbe7a5146f8
Isaac0047 <62448186+Isaac0047@users.noreply.github.com>
2020-12-10 17:26:15 -0600

    Add files via upload

--- Final_image_read.m
+++ Final_image_read.m
@@ -41,76 +41,76 @@ Y(1:out1) = 1;
 
 %% Assign Features
 
-% for i = 1:out+out1
-%     
-%     feature4(i,1) = Perimeter(X(i,:,:));
-%     
-% end
+for i = 1:out+out1
+    
+    feature4(i,1) = Perimeter(X(i,:,:));
+    
+end
 
-% for i = 1:out+out1
-%     
-%     feature1 = stat_feature(X(i,:,:));
-%     feature2 = text_feature(X(i,:,:));
-%     feature3 = shape_feature(X(i,:,:));
-%     feature4 = Perimeter(X(i,:,:));
-%     Feature(i,:) = [feature1 feature2 feature3 feature4];
-% 
-% end
+for i = 1:out+out1
+    
+    feature1 = stat_feature(X(i,:,:));
+    feature2 = text_feature(X(i,:,:));
+    feature3 = shape_feature(X(i,:,:));
+    feature4 = Perimeter(X(i,:,:));
+    Feature(i,:) = [feature1 feature2 feature3 feature4];
+
+end
 
 %% Calculate FFT Feature
 
-% fft   = fft2(X);
-% X_fft = abs(fftshift(fft));
-% 
-% for i = 1:out+out1
-%     
-%     feature1 = stat_feature(X_fft(i,:,:));
-%     Feature_stat(i,:) = [feature1];
-% 
-% end
+fft   = fft2(X);
+X_fft = abs(fftshift(fft));
+
+for i = 1:out+out1
+    
+    feature1 = stat_feature(X_fft(i,:,:));
+    Feature_stat(i,:) = [feature1];
+
+end
 
 %% Calculate wavelet feature
-% N = 1;
-% 
-% for i = 1:1
-%     
-%     X_target = squeeze(X(i,:,:));
-%     [c,s] = wavedec2(X_target,2,'haar');
-% 
-% %     Extract level 1 approximation and detailed coefficients
-%     [H1,V1,D1] = detcoef2('all',c,s,1);
-%     A1         = appcoef2(c,s,'haar',1);
-% 
-% %     Use wcodemat to resacle the coefficents based on absolute value
-%     V1img = wcodemat(V1,255,'mat',1);
-%     H1img = wcodemat(H1,255,'mat',1);
-%     D1img = wcodemat(D1,255,'mat',1);
-%     A1img = wcodemat(A1,255,'mat',1);
+N = 1;
 
-% %     Extract level 2 approximation and detailed coefficients
-%     [H2,V2,D2] = detcoef2('all',c,s,2);
-%     A2         = appcoef2(c,s,'haar',2);
-% 
-%     V2img = wcodemat(V2,255,'mat',1);
-%     H2img = wcodemat(H2,255,'mat',1);
-%     D2img = wcodemat(D2,255,'mat',1);
-%     A2img = wcodemat(A2,255,'mat',1);
+for i = 1:1
     
-%     feature11 = stat_feature(V1img);
-%     feature12 = stat_feature(H1img);
-%     feature13 = stat_feature(D1img);
-%     feature14 = stat_feature(A1img);
-%     
-%     feature21 = stat_feature(V2img);
-%     feature22 = stat_feature(H2img);
-%     feature23 = stat_feature(D2img);
-%     feature24 = stat_feature(A2img);
-%     
-%     Feature_wave1(i,:) = [feature11 feature12 feature13 feature14];
-%     Feature_wave22(N,:) = [feature21 feature22 feature23 feature24];
-%     N = N+1;
+    X_target = squeeze(X(i,:,:));
+    [c,s] = wavedec2(X_target,2,'haar');
 
-% end
+%     Extract level 1 approximation and detailed coefficients
+    [H1,V1,D1] = detcoef2('all',c,s,1);
+    A1         = appcoef2(c,s,'haar',1);
+
+%     Use wcodemat to resacle the coefficents based on absolute value
+    V1img = wcodemat(V1,255,'mat',1);
+    H1img = wcodemat(H1,255,'mat',1);
+    D1img = wcodemat(D1,255,'mat',1);
+    A1img = wcodemat(A1,255,'mat',1);
+
+%     Extract level 2 approximation and detailed coefficients
+    [H2,V2,D2] = detcoef2('all',c,s,2);
+    A2         = appcoef2(c,s,'haar',2);
+
+    V2img = wcodemat(V2,255,'mat',1);
+    H2img = wcodemat(H2,255,'mat',1);
+    D2img = wcodemat(D2,255,'mat',1);
+    A2img = wcodemat(A2,255,'mat',1);
+    
+    feature11 = stat_feature(V1img);
+    feature12 = stat_feature(H1img);
+    feature13 = stat_feature(D1img);
+    feature14 = stat_feature(A1img);
+    
+    feature21 = stat_feature(V2img);
+    feature22 = stat_feature(H2img);
+    feature23 = stat_feature(D2img);
+    feature24 = stat_feature(A2img);
+    
+    Feature_wave1(i,:) = [feature11 feature12 feature13 feature14];
+    Feature_wave22(N,:) = [feature21 feature22 feature23 feature24];
+    N = N+1;
+
+end
 
 %% Temp code for fft
 % 
